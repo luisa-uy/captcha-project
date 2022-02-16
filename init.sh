57bbc97034086a711cda29d0c098364d99e46e3c
Gabriel Núñez <gnuy@pm.me>
2022-02-16 02:15:29 -0300

    Se deshardcodean los paths y se carga el .env

--- init.sh
+++ init.sh
@@ -6,36 +6,37 @@ message () {
 	[ $1 -eq 1 ] && exit 1 || return 0
 }
 
-api_path="./backend"
-front_path="./frontend"
 dummy_env=(
 	"FLASK_APP=app.py"
 	"FLASK_ENV=development"
 	"POSTGRES_USER=admin"
 	"POSTGRES_PASSWORD=admin"
 	"POSTGRES_DB=luisa"
-	"API_PATH=$api_path"
-	"FRONT_PATH=$front_path"
+	"API_PATH=./backend"
+	"FRONT_PATH=./frontend"
 )
 
+# Create dummy .env
+[ ! -f ".env" ] && message 3 ".env file not found, creating it!" \
+	&& printf "%s\n" ${dummy_env[@]} >> .env \
+	|| message 6 "🤔 .env file found, skipping..."
+
+# Load the environment file
+source .env
+
 # Get the frontend project
-[ -d "$front_path" ] && message 6 "🤔 $front_path directory not empty, skipping..." \
-	|| ( ( git clone git@github.com:luisa-uy/captcha-frontend.git $front_path \
-	|| git clone https://github.com/luisa-uy/captcha-frontend.git $front_path ) \
+[ -d "$FRONT_PATH" ] && message 6 "🤔 $FRONT_PATH directory not empty, skipping..." \
+	|| ( ( git clone git@github.com:luisa-uy/captcha-frontend.git $FRONT_PATH \
+	|| git clone https://github.com/luisa-uy/captcha-frontend.git $FRONT_PATH ) \
 	&& message 2 "Frontend repository cloned!" \
 	|| message 1 "Could not clone frontend repository 👀" )
 
 # Get the backend project
-[ -d "$api_path" ] && message 6 "🤔 $api_path directory not empty, skipping..." \
-	|| ( ( git clone git@github.com:luisa-uy/captcha-api.git $api_path \
-	|| git clone https://github.com/luisa-uy/captcha-api.git $api_path ) \
+[ -d "$API_PATH" ] && message 6 "🤔 $API_PATH directory not empty, skipping..." \
+	|| ( ( git clone git@github.com:luisa-uy/captcha-api.git $API_PATH \
+	|| git clone https://github.com/luisa-uy/captcha-api.git $API_PATH ) \
 	&& message 2 "Backend repository cloned!" \
 	|| message 1 "Could not clone backend repository 👀" )
-
-# Set up the environment
-[ ! -f ".env" ] && message 3 ".env file not found, creating it!" \
-	&& printf "%s\n" ${dummy_env[@]} >> .env \
-	|| message 6 "🤔 .env file found, skipping..."
 
 # Make the first build and run `docker-compose up`
 docker-compose up --force-recreate --build -d \
